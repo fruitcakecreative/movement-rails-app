15dc6ad193d147b826faa5b7c613b96330e30ebb
Carly Marsh <carlymarsh@carlymarsh.local>
2025-03-31 23:46:39 -0400

    Remove invalid rename_column migration

--- db/migrate/20250328225410_rename_event_shorter_title_to_even_shorter_title_in_events.rb
+++
@@ -1,5 +0,0 @@
-class RenameEventShorterTitleToEvenShorterTitleInEvents < ActiveRecord::Migration[7.2]
-  def change
-    rename_column :events, :event_shorter_title, :even_shorter_title
-  end
-end
