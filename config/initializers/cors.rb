4d1a47c16b912fbd88521163376d23aeaca0980c
Carly Marsh <carlymarsh@carlymarsh.local>
2025-04-01 16:48:43 -0400

    Trigger redeploy for CORS fix

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,10 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'http://localhost:3000', 'https://movementparties.netlify.app', 'https://www.movementparties.com', 'https://movementparties.com'
+    origins 'http://localhost:3000',
+            'https://movementparties.netlify.app',
+            'https://www.movementparties.com',
+            'https://movementparties.com',
+            /https:\/\/.*--steady-sunshine-e9c556\.netlify\.app/
 
     resource '*',
       headers: :any,
